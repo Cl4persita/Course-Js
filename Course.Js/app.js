3c760a30cfb47e0a59d370a9bd5ff44a62a74dac
Cl4persita <camiloramoscenteno58@gmail.com>
2024-09-03 09:19:00 -0500

    Add files via upload

---
+++ Course.Js/app.js
@@ -0,0 +1,109 @@
+'use strict'
+/*
+document.write("<h1>Hello world</h1>")
+
+//Tipos de datos
+
+//string
+"Hello World"
+'Hello World'
+
+//Number
+10000
+-2.3
+
+//Boolean
+true
+false
+
+//Object
+'Juancho'
+99.99
+35
+
+//Variables
+var name = "Iharita";
+let lastname = "Leon";
+
+name = "Totoilet";
+const PI = 3.1415;
+
+console.log(name)
+console.log(lastname)
+console.log(PI)
+
+//Camelcase
+let userName = "AAAA";
+var lastNameOfPerson = "skibidi";
+
+//Operados
+let NumberOne = 1;
+let NumberTwo = 2;
+let result = NumberOne + NumberTwo;
+console.log(result);
+
+let namee = 'John';
+let lastName = 'Pork';
+let resultt = namee + lastName;
+console.log(resultt);
+
+let password = 'Notoiletsigma'
+let input = 'Notoiletsigm'
+let resulttt = input == password;
+console.log(resulttt)
+
+if (resulttt == true) {
+    console.log('Login Correcto')
+} else {
+    console.log('Login Incorrecto')
+}
+
+
+let score = 70;
+
+if (score > 30) {
+    console.log('Ur good')
+} else {
+    console.log('Ur suck')
+}
+
+
+let typeCard = 'Debito Card'
+
+switch (typeCard) {
+    case 'DaviPlata Card':
+    console.log('Trakaa')
+    break;
+
+    case 'Debit Card':
+    console.log('Yeahhhh')
+    break;
+
+    default:
+        console.log('What r u using')
+        break;
+}
+
+
+//Iteradores
+let count = 1;
+while (count < 50) {
+    console.log(count)
+    count++;
+}
+
+let names = ['Iharita', 'Angelic', 'Estefany'];
+console.log(names.length)
+
+for(let i = 0; i < names.length; i++){
+ console.log(names[i]);
+}
+
+
+//Funciones
+ function greeting(){
+    console.log('Hello')
+ }
+ greeting();
+
+*/
